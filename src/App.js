9f284f8a5b8e0a0fecf115e5c732fc0e9f389381
Yehor Tuz <tuzegor@gmail.com>
2021-12-26 11:52:04 +0200

    Added fetches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  fetchTrendingMovies,
+  fetchSearchMovies,
+  fetchSearchMoviesById,
+  fetchMovieActors,
+  fetchMovieReviews,
+} from './services/movies-api';
 
 function App() {
+  fetchTrendingMovies().then(r => console.log(r));
+  fetchSearchMovies('matrix').then(r => console.log(r));
+  fetchSearchMoviesById('634649').then(r => console.log(r));
+  fetchMovieActors('634649').then(r => console.log(r));
+  fetchMovieReviews('634649').then(r => console.log(r));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/services/movies-api.js
@@ -0,0 +1,30 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '930655cca892db2ef3f0e529ce7dc3f3';
+// https://api.themoviedb.org/3/movie/550?api_key=930655cca892db2ef3f0e529ce7dc3f3
+
+async function baseFetch(url = '', config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Not found'));
+}
+
+export function fetchTrendingMovies() {
+  return baseFetch(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`);
+}
+
+export function fetchSearchMovies(query) {
+  return baseFetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&&query=${query}`,
+  );
+}
+
+export function fetchSearchMoviesById(movieId) {
+  return baseFetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+}
+export function fetchMovieActors(movieId) {
+  return baseFetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+}
+export function fetchMovieReviews(movieId) {
+  return baseFetch(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`);
+}
